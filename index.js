d4340b25f0bf90ee89a10a25d6abafe96e3f53c7
ReflectRFLTK <reflectmainking@gmail.com>
2025-06-09 13:48:51 +0300

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ for (const file of commandFiles) {
 }
 
 
-const rest = new REST({ version: '9' }).setToken(precess.env.token);
+const rest = new REST({ version: '9' }).setToken(config.token);
 
 (async () => {
 	try {
@@ -61,7 +61,7 @@ for (const file of eventFiles) {
 console.log(table.table(data, { header: { alignment: 'center', content: 'Events' }, border: table.getBorderCharacters('norc') }));
 
 
-client.login(precess.env.token);
+client.login(config.token);
 
 const { readingPhoto, sendChannelLlog } = require('./readImage.js');
 

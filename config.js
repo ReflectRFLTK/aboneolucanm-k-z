c427d6bcfbe75ab214e464333dea7dd4c2064cb2
ReflectRFLTK <reflectmainking@gmail.com>
2025-06-09 14:32:32 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
 
-    token: "MTM4MTU0MjgxNjQyOTI0ODUyMg.GSGNWp.OdSb77D5O_TkpErACTVM7ts9_LFn-WGlfQYCjo", //bot tokeniniz
+    token: "MTM4MTU0MjgxNjQyOTI0ODUyMg.G8lQ5F.vy6_Uq9qED2SjXF1Djq44i4P-et2enHf5L9SF4", //bot tokeniniz
     clientId: "1381542816429248522", //Bot idsi
     key: "K88833300888957", //ocr.space key link : https://ocr.space
 
